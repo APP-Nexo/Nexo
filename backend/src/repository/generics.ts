53ae5df85f2c1a666d7358a3ae2f6443483c887e
Hugolelis <hugodelelis05@gmail.com>
2026-03-11 16:48:28 -0300

    (added): db errors and generic queries

---
+++ backend/src/repository/generics.ts
@@ -0,0 +1,45 @@
+import { DatabaseErrors } from "../helpers/errors/database-errors.js"
+
+export class GenericQueries<T> {
+    constructor(private model: any) {}
+
+    async create(data: any): Promise<T> {
+        try {
+        return await this.model.create({ data })
+        } catch {
+        DatabaseErrors.throwQueryFailed()
+        }
+    }
+
+    async findMany(filter = {}): Promise<T[]> {
+        try {
+        return await this.model.findMany({ where: filter })
+        } catch {
+        DatabaseErrors.throwQueryFailed()
+        }
+    }
+
+    async findUnique(where: any): Promise<T | null> {
+        try {
+        return await this.model.findUnique({ where })
+        } catch {
+        DatabaseErrors.throwQueryFailed()
+        }
+    }
+
+    async update(id: string, data: any): Promise<T> {
+        try {
+        return await this.model.update({ where: { id }, data })
+        } catch {
+        DatabaseErrors.throwQueryFailed()
+        }
+    }
+
+    async delete(id: string): Promise<T> {
+        try {
+        return await this.model.delete({ where: { id } })
+        } catch {
+        DatabaseErrors.throwQueryFailed()
+        }
+    }
+}
