3d9382db6ddae520d819889e5f183258f87a8393
Hugolelis <hugodelelis05@gmail.com>
2026-03-30 17:10:31 -0300

    (updated): test unitario

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'node',
-        include: ['tests/**/*.tests.ts'],
+        include: ['tests/**/*.test.ts'],
         coverage: {
             reporter: ['text', 'html']
         }
